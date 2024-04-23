b4a52129a4085967ac6a6a3463103d1f9040cb7b
X3l1m <edibselim1@gmail.com>
2024-04-24 01:47:18 +0200

    ev

--- sources/parsing/parse_words.c
+++ sources/parsing/parse_words.c
@@ -26,31 +26,6 @@ bool	space_present(char *string)
 	return (false);
 }
 
-void	cmd_split_var(t_commands *cmd, char *string)
-{
-	t_token		*new;
-	t_token		*temp;
-	char		**words;
-	int			i;
-
-	new = NULL;
-	words = ft_split(string, ' ');
-	if (!words)
-		return ;
-	cmd->com = ft_strdup(words[0]);
-	if (words[1])
-		new = new_token(ft_strdup(words[1]), NULL, WORD, DEFAULT);
-	temp = new;
-	i = 2;
-	while (words[i++])
-		lst_add_back_tkn(&new, new_token(ft_strdup(words[i]),
-				NULL, WORD, DEFAULT));
-	lst_add_back_tkn(&new, new_token(NULL, NULL, END, DEFAULT));
-	process_args(&new, cmd);
-	lst_clear_tkn(&temp, &free_pointer);
-	free_str_arr(words);
-}
-
 void	parse_word(t_commands **cmd, t_token **list)
 {
 	t_commands	*last;
@@ -65,7 +40,7 @@ void	parse_word(t_commands **cmd, t_token **list)
 		{
 			if (temp->type == VAR && space_present(temp->string))
 			{
-				cmd_split_var(last, temp->string);
+				last->com = ft_strdup(temp->string);
 			}
 			else if ((temp->string && temp->string[0])
 				|| (temp->next->type != WORD && temp->next->type != VAR))
